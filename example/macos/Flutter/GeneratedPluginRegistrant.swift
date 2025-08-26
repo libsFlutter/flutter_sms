5ec3755f943534831d5400b01775019067068bd1
Anton Dodonov <anton.v.dodonov@gmail.com>
2025-08-26 21:55:20 +0700

    feat: example implementation

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_smsussd
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSmsussdPlugin.register(with: registry.registrar(forPlugin: "FlutterSmsussdPlugin"))
+}
