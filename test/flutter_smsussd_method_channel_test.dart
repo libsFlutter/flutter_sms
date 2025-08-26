6969a233c538b319ca0ed8e6f73463c974115f05
Anton Dodonov <anton.v.dodonov@gmail.com>
2025-08-26 21:06:50 +0700

    feat: tests

--- test/flutter_smsussd_method_channel_test.dart
+++ test/flutter_smsussd_method_channel_test.dart
@@ -5,23 +5,144 @@ import 'package:flutter_smsussd/flutter_smsussd_method_channel.dart';
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelFlutterSmsussd platform = MethodChannelFlutterSmsussd();
-  const MethodChannel channel = MethodChannel('flutter_smsussd');
+  group('MethodChannelFlutterSmsussd', () {
+    late MethodChannelFlutterSmsussd plugin;
+    late MethodChannel channel;
 
-  setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
-  });
+    setUp(() {
+      plugin = MethodChannelFlutterSmsussd();
+      channel = plugin.methodChannel;
+    });
 
-  tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
-  });
+    test('getPlatformVersion returns a string', () async {
+      const expected = '42';
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        channel,
+        (MethodCall methodCall) async => expected,
+      );
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
+      final result = await plugin.getPlatformVersion();
+      expect(result, expected);
+    });
+
+    test('sendSms returns true on success', () async {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        channel,
+        (MethodCall methodCall) async => true,
+      );
+
+      final result = await plugin.sendSms(
+        phoneNumber: '+1234567890',
+        message: 'Test message',
+      );
+      expect(result, true);
+    });
+
+    test('sendSms returns false on failure', () async {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        channel,
+        (MethodCall methodCall) async => false,
+      );
+
+      final result = await plugin.sendSms(
+        phoneNumber: '+1234567890',
+        message: 'Test message',
+      );
+      expect(result, false);
+    });
+
+    test('getSmsMessages returns list of messages', () async {
+      final mockMessages = [
+        {
+          'id': '1',
+          'address': '+1234567890',
+          'body': 'Test message 1',
+          'date': 1640995200000,
+          'type': 0,
+        },
+        {
+          'id': '2',
+          'address': '+0987654321',
+          'body': 'Test message 2',
+          'date': 1640995260000,
+          'type': 1,
+        },
+      ];
+
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        channel,
+        (MethodCall methodCall) async => mockMessages,
+      );
+
+      final result = await plugin.getSmsMessages();
+      expect(result.length, 2);
+      expect(result[0].id, '1');
+      expect(result[0].address, '+1234567890');
+      expect(result[0].body, 'Test message 1');
+      expect(result[0].type, SmsType.inbox);
+      expect(result[1].id, '2');
+      expect(result[1].address, '+0987654321');
+      expect(result[1].body, 'Test message 2');
+      expect(result[1].type, SmsType.sent);
+    });
+
+    test('getSmsMessagesByPhoneNumber returns filtered messages', () async {
+      final mockMessages = [
+        {
+          'id': '1',
+          'address': '+1234567890',
+          'body': 'Test message 1',
+          'date': 1640995200000,
+          'type': 0,
+        },
+      ];
+
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        channel,
+        (MethodCall methodCall) async => mockMessages,
+      );
+
+      final result = await plugin.getSmsMessagesByPhoneNumber('+1234567890');
+      expect(result.length, 1);
+      expect(result[0].address, '+1234567890');
+    });
+
+    test('requestSmsPermissions returns true on success', () async {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        channel,
+        (MethodCall methodCall) async => true,
+      );
+
+      final result = await plugin.requestSmsPermissions();
+      expect(result, true);
+    });
+
+    test('hasSmsPermissions returns true when granted', () async {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        channel,
+        (MethodCall methodCall) async => true,
+      );
+
+      final result = await plugin.hasSmsPermissions();
+      expect(result, true);
+    });
+
+    test('hasSmsPermissions returns false when not granted', () async {
+      TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+          .setMockMethodCallHandler(
+        channel,
+        (MethodCall methodCall) async => false,
+      );
+
+      final result = await plugin.hasSmsPermissions();
+      expect(result, false);
+    });
   });
 }
--- test/flutter_smsussd_test.dart
+++ test/flutter_smsussd_test.dart
@@ -1,29 +1,59 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_smsussd/flutter_smsussd.dart';
-import 'package:flutter_smsussd/flutter_smsussd_platform_interface.dart';
-import 'package:flutter_smsussd/flutter_smsussd_method_channel.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-class MockFlutterSmsussdPlatform
-    with MockPlatformInterfaceMixin
-    implements FlutterSmsussdPlatform {
-
-  @override
-  Future<String?> getPlatformVersion() => Future.value('42');
-}
 
 void main() {
-  final FlutterSmsussdPlatform initialPlatform = FlutterSmsussdPlatform.instance;
+  TestWidgetsFlutterBinding.ensureInitialized();
 
-  test('$MethodChannelFlutterSmsussd is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelFlutterSmsussd>());
-  });
+  group('FlutterSmsussd', () {
+    test('getPlatformVersion returns a string', () async {
+      final plugin = FlutterSmsussd();
+      final version = await plugin.getPlatformVersion();
+      expect(version, isA<String>());
+    });
 
-  test('getPlatformVersion', () async {
-    FlutterSmsussd flutterSmsussdPlugin = FlutterSmsussd();
-    MockFlutterSmsussdPlatform fakePlatform = MockFlutterSmsussdPlatform();
-    FlutterSmsussdPlatform.instance = fakePlatform;
+    test('SmsMessage.fromMap creates correct object', () {
+      final map = {
+        'id': '123',
+        'address': '+1234567890',
+        'body': 'Test message',
+        'date': 1640995200000, // 2022-01-01 00:00:00 UTC
+        'type': 1, // sent
+      };
 
-    expect(await flutterSmsussdPlugin.getPlatformVersion(), '42');
+      final message = SmsMessage.fromMap(map);
+
+      expect(message.id, '123');
+      expect(message.address, '+1234567890');
+      expect(message.body, 'Test message');
+      expect(message.date, DateTime.parse('2022-01-01 00:00:00Z'));
+      expect(message.type, SmsType.sent);
+    });
+
+    test('SmsMessage.toMap creates correct map', () {
+      final message = SmsMessage(
+        id: '123',
+        address: '+1234567890',
+        body: 'Test message',
+        date: DateTime.parse('2022-01-01 00:00:00Z'),
+        type: SmsType.inbox,
+      );
+
+      final map = message.toMap();
+
+      expect(map['id'], '123');
+      expect(map['address'], '+1234567890');
+      expect(map['body'], 'Test message');
+      expect(map['date'], 1640995200000);
+      expect(map['type'], 0); // inbox
+    });
+
+    test('SmsType enum values are correct', () {
+      expect(SmsType.inbox.index, 0);
+      expect(SmsType.sent.index, 1);
+      expect(SmsType.draft.index, 2);
+      expect(SmsType.outbox.index, 3);
+      expect(SmsType.failed.index, 4);
+      expect(SmsType.queued.index, 5);
+    });
   });
 }
